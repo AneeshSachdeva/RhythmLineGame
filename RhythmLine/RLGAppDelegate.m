a9b39b9b9b90981bb356f696fc3542fe2e06719c
Aneesh S <AneeshSachdeva@Aneeshs-MacBook-Air.local>
2014-07-28 17:16:29 -0700

    Git test.

--- RhythmLine/RLGAppDelegate.m
+++ RhythmLine/RLGAppDelegate.m
@@ -13,7 +13,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
-    return YES;
+    return YES; //cheese test
 }
 							
 - (void)applicationWillResignActive:(UIApplication *)application
